a5e8775ea653739ff88bc3a1ba3757881d1e107c
Jay Sivakumaran and Kathryn Carr <jsivakumaran@outlook.com>
2015-03-19 16:46:57 -0700

    Add error message if no photo was selected for upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,17 @@
 class PhotosController < ApplicationController
   def create
     @user = User.find(params[:user_id])
+    if params[:photo] == nil
+
+      flash[:alert] = "Please upload a photo"
+      redirect_to :back
+    else
     @photo = Photo.create(photo_params)
-    @photo.user_id = @user.id
-    @photo.save
-    redirect_to user_path(@user)
+      @photo.user_id = @user.id
+      @photo.save
+      flash[:notice] = "Successfully uploaded a photo"
+      redirect_to user_path(@user)
+    end
   end
 
   def new
